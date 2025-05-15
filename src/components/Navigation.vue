8543b14439838d8b25416c0cbb1739572e3e6e39
Andrej Koller <andrejkoller@outlook.com>
2025-05-15 15:25:52 +0200

    refactor: update project structure and remove unused components; add navigation and login views

---
+++ src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'AppNavigation',
+}
+</script>
+
+<template>
+  <div class="navigation-container">
+    <div class="navigation-content">
+      <div class="navigation-links">
+        <ul>
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/login">Login</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="login-container">
+    <div class="login-content">
+      <div class="login-form">
+        <form></form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
