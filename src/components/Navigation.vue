9b6f8a699c07e86c775936cec6a5528003dd7cce
Andrej Koller <andrejkoller@outlook.com>
2025-05-15 16:34:23 +0200

    feat: integrate Vuetify components and add Searchbar; update Navigation layout

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,22 +1,39 @@
 <script>
+import Searchbar from './Searchbar.vue'
+
 export default {
   name: 'AppNavigation',
+  components: {
+    Searchbar,
+  },
 }
 </script>
 
 <template>
   <div class="navigation-container">
     <div class="navigation-content">
-      <div class="navigation-links">
-        <ul>
-          <li>
-            <router-link to="/">Dashboard</router-link>
-          </li>
-          <li>
-            <router-link to="/login">Login</router-link>
-          </li>
-        </ul>
+      <div class="navigation-header">
+        <h1>Spotify Top 50</h1>
       </div>
+      <div class="navigation-body">
+        <div class="navigation-links">
+          <ul>
+            <li>
+              <v-btn variant="outlined">
+                <router-link to="/">Dashboard</router-link>
+              </v-btn>
+            </li>
+            <li>
+              <v-btn variant="outlined">
+                <router-link to="/login">Login</router-link>
+              </v-btn>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="navigation-footer">
+      <Searchbar />
     </div>
   </div>
 </template>
@@ -27,13 +44,23 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 10vh;
   width: 100%;
+  padding: 1vw;
+  gap: 2em;
 }
 
 .navigation-container .navigation-content {
   display: flex;
+  flex-direction: row;
   justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.navigation-container .navigation-content .navigation-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
   align-items: center;
   width: 100%;
 }
@@ -41,7 +68,7 @@ export default {
 .navigation-container .navigation-content .navigation-links {
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: center;
   align-items: center;
   width: 100%;
 }
@@ -49,11 +76,12 @@ export default {
 .navigation-container .navigation-content .navigation-links ul {
   display: flex;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   list-style: none;
   padding: 0;
   margin: 0;
+  width: 100%;
 }
 
 .navigation-container .navigation-content .navigation-links ul li {
@@ -63,6 +91,14 @@ export default {
 .navigation-container .navigation-content .navigation-links ul li a {
   text-decoration: none;
   color: #ffffff;
-  font-size: 24px;
+  font-size: 16px;
+}
+
+.navigation-container .navigation-footer {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 }
 </style>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'AppSearchbar',
+}
+</script>
+
+<template>
+  <div class="searchbar-container">
+    <div class="searchbar-content">
+      <v-text-field label="Search" variant="outlined"></v-text-field>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.searchbar-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.searchbar-container .searchbar-content {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+</style>
